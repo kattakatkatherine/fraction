584a7a3ea4274c876ac89a8d155c248c512decd5
Kat <66186293+katherine-gearhart@users.noreply.github.com>
2021-02-09 13:15:06 -0600

    Added sigfig support.

--- fraction.hs
+++ fraction.hs
@@ -1,11 +1,17 @@
-approx :: Float -> [Integer]
-approx x = [floor (x * fromIntegral (fraction (x - toEnum(floor x)) 0 1 1 1))
-           ,fraction (x - toEnum(floor x)) 0 1 1 1]
+approx :: Float -> Int -> [Int]
+approx x n = [ceiling (x * fromIntegral (denominator n)), denominator n]
+ where denominator n = fraction (x - toEnum(floor x)) n 0 1 1 1
 
--- returns denominator to use
-fraction :: Float -> Integer -> Integer -> Integer -> Integer -> Integer
-fraction x a b c d
+-- returns denominator
+fraction :: Float -> Int -> Int -> Int -> Int -> Int -> Int
+fraction x n a b c d
+ | sigFig x n == sigFig mediant n = b + d
  | x == mediant = b + d
- | x < mediant = fraction x a b (a + c) (b + d)
- | otherwise = fraction x (a + c) (b + d) c d
+ | x < mediant = fraction x n a b (a + c) (b + d)
+ | otherwise = fraction x n (a + c) (b + d) c d
  where mediant = (fromIntegral (a + c))/(fromIntegral (b + d)) :: Float
+
+sigFig :: Float -> Int -> Float
+sigFig x n
+ | length (show x) >= (n + 2) = read (take (n + 2) (show x))
+ | otherwise = x
