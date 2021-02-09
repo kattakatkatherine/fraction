3785f37dffce3face0e7f9ad51b1ac321e48581c
Kat <66186293+katherine-gearhart@users.noreply.github.com>
2021-02-09 15:55:07 -0600

    Update fraction.hs

--- fraction.hs
+++ fraction.hs
@@ -1,17 +1,16 @@
 approx :: Double -> Int -> [Int]
-approx x n = [ceiling (x * fromIntegral (denominator n)), denominator n]
+approx x n = [ceiling $ x * fromIntegral (denominator n), denominator n]
  where denominator n = fraction (x - toEnum(floor x)) n 0 1 1 1
 
 -- returns denominator
 fraction :: Double -> Int -> Int -> Int -> Int -> Int -> Int
 fraction x n a b c d
  | sigFig x n == sigFig mediant n = b + d
- | x == mediant = b + d
  | x < mediant = fraction x n a b (a + c) (b + d)
  | otherwise = fraction x n (a + c) (b + d) c d
  where mediant = (fromIntegral (a + c))/(fromIntegral (b + d)) :: Double
 
 sigFig :: Double -> Int -> Double
 sigFig x n
- | length (show (x + 1)) >= (n + 2) = read (take (n + 2) (show (x + 1)))
+ | length (show (x + 1)) >= n + 2 = read $ take (n + 2) (show (x + 1))
  | otherwise = x
